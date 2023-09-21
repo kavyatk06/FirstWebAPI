a0224ee8e2929398470547af52db0768194dda16
kavya.thiru <kavya.thiru@gavstech.com>
2023-09-21 16:51:48 +0530

    Calc

---
+++ CollegeApp/CollegeApp/Controllers/CollegeController.cs
@@ -0,0 +1,29 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+
+namespace CollegeApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CollegeController : ControllerBase
+	{
+		[HttpGet]
+		public List<Student> GetStudents()
+		{	
+			List<Student> nplist=new List<Student>();
+			Student np = new Student();
+			np.Id = 1;
+			np.StudentName = "std1";
+			np.HouseName = "Lotus";
+			return nplist;
+			np.Id = 2;
+			np.StudentName = "std2";
+			np.HouseName = "Lilly";
+			return nplist;
+
+
+		}
+	}
+}
